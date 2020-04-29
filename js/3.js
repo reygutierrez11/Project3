a06f75423cf05bee4c3ba3c1e27315bb70296472
Rey <42528706+reygutierrez11@users.noreply.github.com>
2020-04-28 23:14:10 -0500

    Add files via upload

---
+++ js/3.js
@@ -0,0 +1,36 @@
+function themap()
+{
+    var coordinates = {lat: 41.957951, lng: -87.696581}
+    var map = new google.maps.Map(document.getElementById('gmaps'),
+    {center: coordinates,
+    zoom: 20
+    });
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function displayslides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block" ;
+    dots[slideIndex-1].className += " active";
+}
+
